f9c80d0b00d499cac4d1a0dee1c7cbcb32929ea2
Danny <dannyconcepcion01@gmail.com>
2020-09-04 19:59:09 -0400

    Agregar archivos de proyecto.

---
+++ Area_triangulo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este programa calcula el area de un triangulo");
+            double bas, altura,area;
+
+            Console.WriteLine("Digite la base del triangulo");
+            bas = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite la altura del triangulo");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+           area= (bas*altura)/ 2;
+
+            Console.WriteLine("El area del triangulo es  :" + area);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
